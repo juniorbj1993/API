8cb0a4a1304a6a427ab8cee88628fc68ec3a64da
jbjunior <juniorbj1993@gmail.com>
2019-04-07 17:28:30 -0300

    add politicas

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::group(['prefix' =>'api'], function () {
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('', ['uses'=>'ApiRestController@allUsers']);
-        Route::get('{id}',['uses'=>'ApiRestController@getUsers']);
-        Route::post('', ['uses'=>'ApiRestController@saveUsers']);
-        Route::put('{id}', ['uses'=>'ApiRestController@updateUsers']);
-        Route::delete('{id}',['uses'=>'ApiRestController@deleteUsers'] );
-    });
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/politica','SiteController@politica');
+
+// Route::group(['prefix' =>'api'], function () {
+//     Route::group(['prefix' => 'users'], function () {
+//         Route::get('', ['uses'=>'ApiRestController@allUsers']);
+//         Route::get('{id}',['uses'=>'ApiRestController@getUsers']);
+//         Route::post('', ['uses'=>'ApiRestController@saveUsers']);
+//         Route::put('{id}', ['uses'=>'ApiRestController@updateUsers']);
+//         Route::delete('{id}',['uses'=>'ApiRestController@deleteUsers'] );
+//     });
+// });
